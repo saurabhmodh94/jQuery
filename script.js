48971c8b4922ad9cbbb5ee28f0c6a43d6cffc79a
Saurabh Modh <saurabh.modh@hashedin.com>
2017-02-02 19:41:44 +0530

    Event Handlers: hover

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-function print_console() {
+$(document).ready(function(){
+	$('#randomDiv').hover(function(){
+		console.log("mouse hover.");
+	},function(){
+		console.log("mouse not hover.")
+	});
+});
+/*function print_console() {
     console.log("Saurabh");
-};
+};*/

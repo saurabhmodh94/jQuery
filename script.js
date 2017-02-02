68cd7f3beb28c0754a0c00ac48359b286284f2cd
Saurabh Modh <saurabh.modh@hashedin.com>
2017-02-02 19:58:17 +0530

    Event Handlers 2

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
-	$('div.a').html('123');
-	$('span.a').html('456');
+	/*Other EventHandlers:
+		// change
+		// after
+		// before
+		// insertAfter
+		// insertBefore
+		// next
+		// prev
+		// css
+		// eq(0).css
+		// eq(0).siblings*/
+	// $('div.a').html('123');
+	// $('span.a').html('456');
 	//selectors: ('tag')('#id')('.class')('tag#id')('tag.class')
 	/*$('#randomDiv').hover(function(){
 		console.log("mouse hover.");

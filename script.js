c30b841d3c8fd26c23a0982c9bbb0bc6e7e4f18f
Saurabh Modh <saurabh.modh@hashedin.com>
2017-02-02 19:49:21 +0530

    selectors

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 $(document).ready(function(){
-	$('#randomDiv').hover(function(){
+	$('div.a').html('123');
+	$('span.a').html('456');
+	//selectors: ('tag')('#id')('.class')('tag#id')('tag.class')
+	/*$('#randomDiv').hover(function(){
 		console.log("mouse hover.");
 	},function(){
 		console.log("mouse not hover.")
-	});
+	});*/
 });
 /*function print_console() {
     console.log("Saurabh");
